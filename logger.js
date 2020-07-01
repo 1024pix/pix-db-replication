38d400e650440448660daa101abb84927ba1c222
Christophe Charles <ccharles@octo.com>
2020-07-01 09:26:56 +0200

    Create bunyan logger

---
+++ logger.js
@@ -0,0 +1,10 @@
+const bunyan = require('bunyan');
+
+const logger = bunyan.createLogger({
+  name: 'pix-db-replication',
+  stream: process.stdout,
+  level: 'info'
+});
+
+
+module.exports = logger;
