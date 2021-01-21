f7d5564bf9b43cd1fb7d9874558c931be4d3e4b4
François de Metz <francois.de-metz@pix.fr>
2021-01-21 18:04:42 +0100

    Add queue events logger
    Co-authored-by: Alexandre Coin <alexandre.coin@pix.fr>

--- src/replication_job.js
+++ src/replication_job.js
@@ -13,9 +13,28 @@ const parisTimezone = 'Europe/Paris';
 const extractConfigurationFromEnvironment = require('./extract-configuration-from-environment');
 const configuration = extractConfigurationFromEnvironment();
 
-const replicationQueue = new Queue('Replication queue', configuration.REDIS_URL);
-const airtableReplicationQueue = new Queue('Airtable replication queue', configuration.REDIS_URL);
-const incrementalReplicationQueue = new Queue('Increment replication queue', configuration.REDIS_URL);
+const replicationQueue = addQueueEventsListeners(new Queue('Replication queue', configuration.REDIS_URL));
+const airtableReplicationQueue = addQueueEventsListeners(new Queue('Airtable replication queue', configuration.REDIS_URL));
+const incrementalReplicationQueue = addQueueEventsListeners(new Queue('Increment replication queue', configuration.REDIS_URL));
+
+function addQueueEventsListeners(queue) {
+  return queue
+    .on('error', function(error) {
+      logger.error(`Error in ${queue.name}: ${error}`);
+    })
+    .on('active', function(job) {
+      logger.info(`Starting job in ${queue.name}: ${job.id}`);
+    })
+    .on('stalled', function(job) {
+      logger.error(`Stalled job in ${queue.name}: ${job.id}`);
+    })
+    .on('completed', function(job) {
+      logger.info(`Completed job in ${queue.name}: ${job.id}`);
+    })
+    .on('failed', function(job, err) {
+      logger.error(`Failed job in ${queue.name}: ${job.id} ${err}`);
+    });
+}
 
 async function main() {
   initSentry(configuration);
