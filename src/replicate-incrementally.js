0ee4f6e2caeef0ce2e1c256708b39d76113b739b
yahya youness <yahyayounes@hotmail.fr>
2021-06-21 18:10:58 +0200

    keep only ke-snapshots for replication

--- src/replicate-incrementally.js
+++ src/replicate-incrementally.js
@@ -22,22 +22,6 @@ async function run(configuration) {
 
   logger.info('Start incremental replication');
 
-  const maxAnswerIdStr = await execStdOut('psql', [configuration.TARGET_DATABASE_URL, '--tuples-only', '--command', 'SELECT MAX(id) FROM answers']);
-  const answersLastRecordIndexTargetBeforeReplication = parseInt(maxAnswerIdStr);
-  logger.info('Answers last record index target ' + answersLastRecordIndexTargetBeforeReplication);
-
-  if (isNaN(answersLastRecordIndexTargetBeforeReplication)) {
-    throw new Error('Answers table must not be empty on target database');
-  }
-
-  const maxKEIdStr = await execStdOut('psql', [configuration.TARGET_DATABASE_URL, '--tuples-only', '--command', 'SELECT MAX(id) FROM "knowledge-elements"']);
-  const kELastRecordIndexTargetBeforeReplication = parseInt(maxKEIdStr);
-  logger.info('KE last record index target ' + kELastRecordIndexTargetBeforeReplication);
-
-  if (isNaN(kELastRecordIndexTargetBeforeReplication)) {
-    throw new Error('Knowledge-elements table must not be empty on target database');
-  }
-
   const maxKESnapshotsIdStr = await execStdOut('psql', [configuration.TARGET_DATABASE_URL, '--tuples-only', '--command', 'SELECT MAX(id) FROM "knowledge-element-snapshots"']);
   const kESnapshotsLastRecordIndexTargetBeforeReplication = parseInt(maxKESnapshotsIdStr);
   logger.info('KE last record index target ' + kESnapshotsLastRecordIndexTargetBeforeReplication);
@@ -49,14 +33,6 @@ async function run(configuration) {
   logger.info('Start COPY FROM/TO through STDIN/OUT');
 
   const tablesAndLastRecordIndex = [
-    {
-      name: 'answers',
-      lastRecordIndex: answersLastRecordIndexTargetBeforeReplication,
-    },
-    {
-      name: 'knowledge-elements',
-      lastRecordIndex: kELastRecordIndexTargetBeforeReplication,
-    },
     {
       name: 'knowledge-element-snapshots',
       lastRecordIndex: kESnapshotsLastRecordIndexTargetBeforeReplication,
@@ -95,7 +71,6 @@ async function run(configuration) {
     ]);
     const lastRecordIndexTargetAfterReplication = parseInt(maxIdStrAfterReplication);
     logger.info(`${table.name} last record index target after replication ` + lastRecordIndexTargetAfterReplication);
-
     logger.info(`Total of ${table.name} imported ` + (lastRecordIndexTargetAfterReplication - table.lastRecordIndex));
   }
 
