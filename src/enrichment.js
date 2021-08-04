da46fb7fcbd56c9d425c41c14584c78c5996d186
yahya youness <yahyayounes@hotmail.fr>
2021-08-04 16:28:04 +0200

    [SR] rename _getIncrementalTables to _getTablesToNotBeEnriched
    Co-authored-by: karam <karam.haytham.mohammed@pix.fr>
    Co-authored-by: Alexandre <alexandre.coin@pix.fr>

--- src/enrichment.js
+++ src/enrichment.js
@@ -6,31 +6,31 @@ const toPairs = require('lodash/toPairs');
 
 async function add(configuration) {
   await runDBOperation(async (client) => {
-    const incrementalTables = _getIncrementalTables(configuration);
-    if (!incrementalTables.includes('knowledge-elements')) {
+    const tablesToNotBeEnriched = _getTablesToNotBeEnriched(configuration);
+    if (!tablesToNotBeEnriched.includes('knowledge-elements')) {
       logger.info('CREATE INDEX "knowledge-elements_createdAt_idx" - Started');
       await client.query('CREATE INDEX "knowledge-elements_createdAt_idx" on "knowledge-elements" (cast("createdAt" AT TIME ZONE \'UTC+1\' as date) DESC)');
       logger.info('CREATE INDEX "knowledge-elements_createdAt_idx" - Ended');
 
     }
-    if (!incrementalTables.includes('knowledge-element-snapshots')) {
+    if (!tablesToNotBeEnriched.includes('knowledge-element-snapshots')) {
       logger.info('CREATE INDEX "knowledge-element-snapshots_snappedAt_idx" - Started');
       await client.query('CREATE INDEX "knowledge-element-snapshots_snappedAt_idx" on "knowledge-element-snapshots" (cast("snappedAt" AT TIME ZONE \'UTC+1\' as date) DESC)');
       logger.info('CREATE INDEX "knowledge-element-snapshots_snappedAt_idx" - Ended');
 
     }
-    if (!incrementalTables.includes('answers')) {
+    if (!tablesToNotBeEnriched.includes('answers')) {
       logger.info('CREATE INDEX "answers_challengeId_idx" - Started');
       await client.query('CREATE INDEX "answers_challengeId_idx" on "answers" ("challengeId")');
       logger.info('CREATE INDEX "answers_challengeId_idx" - Ended');
     }
 
-    if (!incrementalTables.includes('users')) {
+    if (!tablesToNotBeEnriched.includes('users')) {
       logger.info('CREATE INDEX "users_createdAt_idx" - Started');
       await client.query('CREATE INDEX "users_createdAt_idx" on "users" (cast("createdAt" AT TIME ZONE \'UTC+1\' as date) DESC)');
       logger.info('CREATE INDEX "users_createdAt_idx" - Ended');
     }
-    if (!incrementalTables.includes('schooling-registrations')) {
+    if (!tablesToNotBeEnriched.includes('schooling-registrations')) {
       logger.info('CREATE VIEW students - Started');
       await client.query('CREATE VIEW students AS SELECT * FROM "schooling-registrations"');
       logger.info('CREATE VIEW students - Ended');
@@ -38,7 +38,7 @@ async function add(configuration) {
   }, configuration);
 }
 
-function _getIncrementalTables(configuration) {
+function _getTablesToNotBeEnriched(configuration) {
   const tablePairs = toPairs(configuration.BACKUP_MODE);
   return tablePairs
     .filter(([_, mode]) => mode === 'incremental' || mode === 'none')
