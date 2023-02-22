891e64a5f4193817444be4c46ea727ba9b99bb1f
François de Metz <francois.de-metz@pix.fr>
2023-02-22 16:25:07 +0100

    Import lcms data table by table

--- src/replicate-learning-content.js
+++ src/replicate-learning-content.js
@@ -101,11 +101,11 @@ const tables = [{
 async function fetchAndSaveData(configuration) {
   const learningContent = await lcmsClient.getLearningContent(configuration);
   if (learningContent) {
-    await Promise.all(tables.map(async (table) => {
+    for await (const table of tables) {
       await databaseHelper.dropTable(table.name, configuration);
       await databaseHelper.createTable(table, configuration);
       await databaseHelper.saveLearningContent(table, learningContent[table.name], configuration);
-    }));
+    }
   }
 }
 
