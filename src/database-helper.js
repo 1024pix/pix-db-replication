47e44acd657ab9adc46fe2afdf1049dd15e9b87c
François de Metz <francois.de-metz@pix.fr>
2023-02-22 16:23:33 +0100

    Import values by chunk

--- src/database-helper.js
+++ src/database-helper.js
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const format = require('pg-format');
+const _ = require('lodash');
 
 const { PrimaryKeyNotNullConstraintError } = require('./errors');
 const learningContentHelper = require('./learning-content-helper');
@@ -41,13 +42,15 @@ async function saveLearningContent(table, learningContent, configuration) {
   if (learningContent.length) {
     await runDBOperation(async (client) => {
       const fieldNames = ['id'].concat(table.fields.map((field) => field.name));
-      const values = _computeValuesToInsert(table, learningContent);
-      if (_allValuesHavePrimaryKey(values)) {
-        await _insertValues(table.name, fieldNames, values, client);
-      }
-      else {
-        throw new PrimaryKeyNotNullConstraintError(table.name);
-      }
+      for await (const learningContentChunk of _.chunk(learningContent, 2000)) {
+        const values = _computeValuesToInsert(table, learningContentChunk);
+        if (_allValuesHavePrimaryKey(values)) {
+          await _insertValues(table.name, fieldNames, values, client);
+        }
+        else {
+          throw new PrimaryKeyNotNullConstraintError(table.name);
+        }
+      };
     }, configuration);
   }
 }
@@ -65,9 +68,9 @@ function _allValuesHavePrimaryKey(values) {
   });
 }
 
-async function _insertValues(tableName, fieldNames, values, client) {
+function _insertValues(tableName, fieldNames, values, client) {
   const saveQuery = format('INSERT INTO %I (%I) VALUES %L', tableName, fieldNames, values);
-  await client.query(saveQuery);
+  return client.query(saveQuery);
 }
 
 module.exports = {
