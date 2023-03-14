45ed07db26ae99f0b3063e0f4358956cfe34f28c
Mickaël Alibert <mickael.alibert@pix.fr>
2023-03-14 16:07:14 +0100

    :sparkles: Fetch CPF formations list from open data

---
+++ cpf/scripts/fetch-cpf-formations.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -eu
+
+dbclient-fetcher pgsql "$PG_CLIENT_VERSION"
+
+TABLE_NAME='pole_formation_cpf_list'
+DATASET_URL='https://opendata.caissedesdepots.fr/api/records/1.0/search/?dataset=moncompteformation_catalogueformation&rows=2000&q=pix&facet=nom_departement&facet=nom_region&facet=rsrncp&facet=libelle_niveau_sortie_formation&facet=libelle_nsf_1&facet=code_certifinfo&facet=siret&facet=code_region&refine.rsrncp=RS'
+OUTPUT_FILE='/tmp/exportcpf.csv'
+
+psql "$DATABASE_URL" \
+    -c "DROP TABLE IF EXISTS $TABLE_NAME; CREATE TABLE IF NOT EXISTS $TABLE_NAME (record_id TEXT, date_extract DATE, nom_of TEXT, intitule_formation TEXT, numero_formation TEXT)"
+
+curl "$DATASET_URL" \
+> /tmp/export.json
+
+jq -r '.records[]|
+    [ 
+        .recordid,
+        .fields.date_extract, 
+        .fields.nom_of, 
+        .fields.intitule_formation, 
+        .fields.numero_formation
+    ]|
+    join(";")' /tmp/export.json \
+> "$OUTPUT_FILE"
+
+psql "$DATABASE_URL" -c "\copy $TABLE_NAME FROM '$OUTPUT_FILE' DELIMITER ';';"
