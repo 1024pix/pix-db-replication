bc0b8e01525230f18d9391079d26a1f8e4c5b0ac
Mickaël Alibert <mickael.alibert@pix.fr>
2023-03-23 16:30:56 +0100

    :sparkles: Formation fetching script data now uses correct url by default

---
+++ cpf/create-table.sql
@@ -0,0 +1,2 @@
+-- .recordid, .fields.nom_of, .fields.intitule_formation, .fields.numero_formation
+CREATE TABLE external_opendata_caisse_des_depots_cpf_list (record_id TEXT, date_extract DATE, nom_of TEXT, intitule_formation TEXT, numero_formation TEXT)
