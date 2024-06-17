d7d825564f677fcedfa2d6277d8f7722aba3506f
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-17 17:33:46 +0200

    tests: add missing notifications tests
    Co-authored-by: Gudsfile <gudsfile@protonmail.com>

---
+++ test/integration/steps/notification_test.js
@@ -0,0 +1,30 @@
+import { expect, catchErr, nock } from '../../test-helper.js';
+import {run} from '../../../src/steps/notification.js';
+
+
+describe('Integration | Steps | notification.js', () => {
+  describe('run', function() {
+    it('should call all notification urls', async function () {
+      const configuration = {
+        NOTIFICATION_URLS: [
+          'http://example.net/webhook1',
+          'http://example.net/webhook2',
+        ]
+      };
+
+      const scopeWebhook1 = nock('http://example.net')
+        .post('/webhook1')
+        .reply(200, {});
+
+      const scopeWebhook2 = nock('http://example.net')
+        .post('/webhook2')
+        .reply(200, {});
+
+
+      await run(configuration);
+
+      expect(scopeWebhook1.isDone()).to.be.true;
+      expect(scopeWebhook2.isDone()).to.be.true;
+    });
+  });
+});
