119d41a78e01b3ec949d701a108a34c25991be06
Guillaume Lagorce <guillaume@hey-gul.com>
2025-01-26 11:11:23 +0100

    tech: update parse-replication-logs script

--- utils/parse-replication-logs.js
+++ utils/parse-replication-logs.js
@@ -1,10 +1,21 @@
 'use strict';
-const fs = require('fs');
+import fs from 'fs';
 
 let DATE;
 const START_BACKUP = 'Start create Backup';
-const START_REPLICATION = 'Start restore';
-const START_ENRICHMENT = 'Restore done';
+const END_BACKUP = 'End create Backup';
+const START_DROP = 'Start drop Objects AndRestoreBackup';
+const END_DROP = 'End drop Objects AndRestoreBackup';
+const START_CREATE_VIEW = 'Start create view';
+const END_CREATE_VIEW = 'End create view';
+const START_RESTORE = 'Start restore';
+const END_RESTORE = 'Restore done';
+const START_INCREMENTAL = 'Start incremental replication';
+const END_INCREMENTAL = 'Incremental replication done';
+const START_LCMS = 'learningContent.run - Started';
+const END_LCMS = 'learningContent.run - Ended';
+const START_UPDATE_AUTH_METHODS = 'UPDATE \\"authentication-methods\\" - Started';
+const END_UPDATE_AUTH_METHODS = 'UPDATE \\"authentication-methods\\" - Ended';
 
 function log(args) {
   // eslint-disable-next-line no-console
@@ -13,7 +24,7 @@ function log(args) {
 
 function _printPrettyTimeElapsedBetweenTwoDates(olderDate, date) {
   const secondsElapsed = _getSecondsElapsedBetweenTwoDates(olderDate, date);
-  return _printPrettyTimeElapsed(secondsElapsed);
+  return `${_printPrettyTimeElapsed(secondsElapsed)} (de ${olderDate?.toLocaleTimeString()} à ${date?.toLocaleTimeString()})`;
 }
 
 function _printPrettyTimeElapsed(secondsElapsed) {
@@ -25,7 +36,7 @@ function _printPrettyTimeElapsed(secondsElapsed) {
 }
 
 function _getSecondsElapsedBetweenTwoDates(olderDate, date) {
-  return (date.getTime() - olderDate.getTime()) / 1000;
+  return (date?.getTime() - olderDate?.getTime()) / 1000;
 }
 
 function _extractTimestampFromLogLine(logLine) {
@@ -40,163 +51,8 @@ function _filterLinesInDate(logLines) {
 
 function _extractTimestampFromContent(logLines, content) {
   const logLineWithTimestamp = logLines.find((line) => line.includes(content));
+  if (!logLineWithTimestamp) return undefined;
   return _extractTimestampFromLogLine(logLineWithTimestamp);
-}
-
-function _editItemIfComplete(collection, key) {
-  if (collection[key].endTimestamp && collection[key].startTimestamp) {
-    collection[key] = {
-      operation: collection[key].operation,
-      elapsed: _getSecondsElapsedBetweenTwoDates(collection[key].startTimestamp, collection[key].endTimestamp),
-    };
-  }
-}
-
-function _findItemsInLogLines(logLines) {
-  const findingItems = {};
-  const rxLaunch = /.* launching item ([0-9]*) (.*)/g;
-  const rxFinished = /.* finished item ([0-9]*) .*/g;
-  for (const logLine of logLines) {
-    rxLaunch.lastIndex = 0;
-    rxFinished.lastIndex = 0;
-    const matchLaunch = rxLaunch.exec(logLine);
-    const matchFinished = rxFinished.exec(logLine);
-    if (matchLaunch) {
-      const line = matchLaunch[0];
-      const opId = matchLaunch[1].toString();
-      const op = matchLaunch[2];
-      if (!findingItems[opId]) {
-        findingItems[opId] = {};
-      }
-      findingItems[opId].startTimestamp = _extractTimestampFromLogLine(line);
-      findingItems[opId].operation = op;
-      _editItemIfComplete(findingItems, opId);
-    }
-    if (matchFinished) {
-      const line = matchFinished[0];
-      const opId = matchFinished[1].toString();
-      if (!findingItems[opId]) {
-        findingItems[opId] = {};
-      }
-      findingItems[opId].endTimestamp = _extractTimestampFromLogLine(line);
-      _editItemIfComplete(findingItems, opId);
-    }
-  }
-
-  return findingItems;
-}
-
-function _categorize(foundItems) {
-  const sequenceItems = {};
-  const tableDataItems = {};
-  const constraintItems = {};
-  const indexItems = {};
-  const fkConstraintItems = {};
-  sequenceItems.totalElapsed = 0;
-  sequenceItems.operations = [];
-  tableDataItems.totalElapsed = 0;
-  tableDataItems.operations = [];
-  constraintItems.totalElapsed = 0;
-  constraintItems.operations = [];
-  indexItems.totalElapsed = 0;
-  indexItems.operations = [];
-  fkConstraintItems.totalElapsed = 0;
-  fkConstraintItems.operations = [];
-  for (const operationInfo of Object.values(foundItems)) {
-    if (operationInfo.operation.startsWith('FK CONSTRAINT')) {
-      fkConstraintItems.totalElapsed += operationInfo.elapsed;
-      fkConstraintItems.operations.push(operationInfo);
-    }
-    if (operationInfo.operation.startsWith('CONSTRAINT')) {
-      constraintItems.totalElapsed += operationInfo.elapsed;
-      constraintItems.operations.push(operationInfo);
-    }
-    if (operationInfo.operation.startsWith('INDEX')) {
-      indexItems.totalElapsed += operationInfo.elapsed;
-      indexItems.operations.push(operationInfo);
-    }
-    if (operationInfo.operation.startsWith('SEQUENCE')) {
-      sequenceItems.totalElapsed += operationInfo.elapsed;
-      sequenceItems.operations.push(operationInfo);
-    }
-    if (operationInfo.operation.startsWith('TABLE DATA')) {
-      tableDataItems.totalElapsed += operationInfo.elapsed;
-      tableDataItems.operations.push(operationInfo);
-    }
-  }
-
-  return {
-    sequenceItems,
-    tableDataItems,
-    constraintItems,
-    indexItems,
-    fkConstraintItems,
-  };
-}
-
-function _sortByElapsed(categoryItems) {
-  categoryItems.operations = categoryItems.operations.sort(_compareOperation);
-  return categoryItems;
-}
-
-function _compareOperation(operationA, operationB) {
-  if (operationA.elapsed > operationB.elapsed) {
-    return -1;
-  } else if (operationA.elapsed < operationB.elapsed) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-function _categorizeAndSortByElapsed(foundItems) {
-  const {
-    sequenceItems,
-    tableDataItems,
-    constraintItems,
-    indexItems,
-    fkConstraintItems,
-  } = _categorize(foundItems);
-
-  return {
-    sequenceItems: _sortByElapsed(sequenceItems),
-    tableDataItems: _sortByElapsed(tableDataItems),
-    constraintItems: _sortByElapsed(constraintItems),
-    indexItems: _sortByElapsed(indexItems),
-    fkConstraintItems: _sortByElapsed(fkConstraintItems),
-  };
-}
-
-function _do(logLines) {
-  const foundItems = _findItemsInLogLines(logLines);
-  const {
-    sequenceItems,
-    tableDataItems,
-    constraintItems,
-    indexItems,
-    fkConstraintItems,
-  } = _categorizeAndSortByElapsed(foundItems);
-
-  log(`FK CONSTRAINT total duration : ${_printPrettyTimeElapsed(fkConstraintItems.totalElapsed)}`);
-  for (let i = 0; i < 3 && fkConstraintItems.operations.length > 0; ++i) {
-    log(`\t${fkConstraintItems.operations[i].operation} : ${_printPrettyTimeElapsed(fkConstraintItems.operations[i].elapsed)}`);
-  }
-  log(`CONSTRAINT total duration : ${_printPrettyTimeElapsed(constraintItems.totalElapsed)}`);
-  for (let i = 0; i < 3 && constraintItems.operations.length > 0; ++i) {
-    log(`\t${constraintItems.operations[i].operation} : ${_printPrettyTimeElapsed(constraintItems.operations[i].elapsed)}`);
-  }
-  log(`INDEX total duration : ${_printPrettyTimeElapsed(indexItems.totalElapsed)}`);
-  for (let i = 0; i < 3 && indexItems.operations.length > 0; ++i) {
-    log(`\t${indexItems.operations[i].operation} : ${_printPrettyTimeElapsed(indexItems.operations[i].elapsed)}`);
-  }
-  log(`SEQUENCE total duration : ${_printPrettyTimeElapsed(sequenceItems.totalElapsed)}`);
-  for (let i = 0; i < 3 && sequenceItems.operations.length > 0; ++i) {
-    log(`\t${sequenceItems.operations[i].operation} : ${_printPrettyTimeElapsed(sequenceItems.operations[i].elapsed)}`);
-  }
-  log(`TABLE DATA total duration : ${_printPrettyTimeElapsed(tableDataItems.totalElapsed)}`);
-  for (let i = 0; i < 3 && tableDataItems.operations.length > 0; ++i) {
-    log(`\t${tableDataItems.operations[i].operation} : ${_printPrettyTimeElapsed(tableDataItems.operations[i].elapsed)}`);
-  }
 }
 
 async function main() {
@@ -207,15 +63,31 @@ async function main() {
     const logs = fs.readFileSync(logfile, 'utf8');
     let logLines = logs.split('\n');
     logLines = _filterLinesInDate(logLines);
+
     const startBackupTimestamp = _extractTimestampFromContent(logLines, START_BACKUP);
-    const startReplicationTimestamp = _extractTimestampFromContent(logLines, START_REPLICATION);
-    const startEnrichmentTimestamp = _extractTimestampFromContent(logLines, START_ENRICHMENT);
-    const endTimestamp = _extractTimestampFromLogLine(logLines.slice(-1));
-    log(`Durée de création du backup: ${_printPrettyTimeElapsedBetweenTwoDates(startBackupTimestamp, startReplicationTimestamp)}`);
-    log(`Durée de réplication: ${_printPrettyTimeElapsedBetweenTwoDates(startReplicationTimestamp, startEnrichmentTimestamp)}`);
-    log(`Durée de l'enrichissement: ${_printPrettyTimeElapsedBetweenTwoDates(startEnrichmentTimestamp, endTimestamp)}`);
-    log(`Durée totale: ${_printPrettyTimeElapsedBetweenTwoDates(startBackupTimestamp, endTimestamp)}`);
-    _do(logLines);
+    const endBackupTimestamp = _extractTimestampFromContent(logLines, END_BACKUP);
+    const startDropTimestamp = _extractTimestampFromContent(logLines, START_DROP);
+    const endDropTimestamp = _extractTimestampFromContent(logLines, END_DROP);
+    const startCreateViewTimestamp = _extractTimestampFromContent(logLines, START_CREATE_VIEW);
+    const endCreateViewTimestamp = _extractTimestampFromContent(logLines, END_CREATE_VIEW);
+    const startRestoreTimestamp = _extractTimestampFromContent(logLines, START_RESTORE);
+    const endRestoreTimestamp = _extractTimestampFromContent(logLines, END_RESTORE);
+    const startIncrementalTimestamp = _extractTimestampFromContent(logLines, START_INCREMENTAL);
+    const endIncrementalTimestamp = _extractTimestampFromContent(logLines, END_INCREMENTAL);
+    const startLearningContentTimestamp = _extractTimestampFromContent(logLines, START_LCMS);
+    const endLearningContentTimestamp = _extractTimestampFromContent(logLines, END_LCMS);
+    const startUpdateAuthMethods = _extractTimestampFromContent(logLines, START_UPDATE_AUTH_METHODS);
+    const endUpdateAuthMethods = _extractTimestampFromContent(logLines, END_UPDATE_AUTH_METHODS);
+
+    log(`# Dans le fichier ${logfile}`);
+    log(`Durée de création du backup: ${_printPrettyTimeElapsedBetweenTwoDates(startBackupTimestamp, endBackupTimestamp)}`);
+    log(`Durée du drop des tables: ${_printPrettyTimeElapsedBetweenTwoDates(startDropTimestamp, endDropTimestamp)}`);
+    log(`Durée du restore: ${_printPrettyTimeElapsedBetweenTwoDates(startRestoreTimestamp, endRestoreTimestamp)}`);
+    log(`Durée de la création des vues: ${_printPrettyTimeElapsedBetweenTwoDates(startCreateViewTimestamp, endCreateViewTimestamp)}`);
+    log(`Durée de la mise à jour des authentication-methods: ${_printPrettyTimeElapsedBetweenTwoDates(startUpdateAuthMethods, endUpdateAuthMethods)}`);
+    log(`Durée de l'incrémentale: ${_printPrettyTimeElapsedBetweenTwoDates(startIncrementalTimestamp, endIncrementalTimestamp)}`);
+    log(`Durée du Learning Content: ${_printPrettyTimeElapsedBetweenTwoDates(startLearningContentTimestamp, endLearningContentTimestamp)}`);
+    log(`Durée totale: ${_printPrettyTimeElapsedBetweenTwoDates(startBackupTimestamp, endLearningContentTimestamp)}`);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('\n', error);
@@ -224,15 +96,13 @@ async function main() {
   }
 }
 
-if (require.main === module) {
-  main().then(
+main().then(
+  // eslint-disable-next-line n/no-process-exit
+  () => process.exit(0),
+  (err) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
     // eslint-disable-next-line n/no-process-exit
-    () => process.exit(0),
-    (err) => {
-      // eslint-disable-next-line no-console
-      console.error(err);
-      // eslint-disable-next-line n/no-process-exit
-      process.exit(1);
-    },
-  );
-}
+    process.exit(1);
+  },
+);
