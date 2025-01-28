75c88db3b8d7f2fac9ff8f33d3a7c1b7812810de
Berengere <berengere.claudeau@pix.fr>
2025-01-28 17:55:44 +0100

    fix lint

--- src/steps/learning-content/index.js
+++ src/steps/learning-content/index.js
@@ -132,7 +132,7 @@ const tables = [{
     { name: 'format', type: 'text' },
     { name: 'link', type: 'text' },
     { name: 'source', type: 'text' },
-    { name: 'title', type: 'text' },        
+    { name: 'title', type: 'text' },
     { name: 'tutorialForSkills', type: 'text []', isArray: true },
     { name: 'furtherInformation', type: 'text []', isArray: true },
     { name: 'locale', type: 'text' },
