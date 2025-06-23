31b7f0497984824883153d2ceb787c5462678c85
Laura Bergoens <laura@bergoens.fr>
2025-06-23 11:17:32 +0200

    tech: more explicit message when item has no primary key

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,6 @@
 class PrimaryKeyNotNullConstraintError extends Error {
   constructor(tableName) {
-    const message = `At least one item to insert in table ${tableName} has no property at all`;
+    const message = `At least one item to insert in table ${tableName} has no primary key`;
     super(message);
   }
 }
