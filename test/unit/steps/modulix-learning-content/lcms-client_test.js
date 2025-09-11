867182ad1b5d88a11d98863de3e20d44a1d06df9
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-09-11 13:55:57 +0200

    feat: update list of modules

--- test/unit/steps/modulix-learning-content/lcms-client_test.js
+++ test/unit/steps/modulix-learning-content/lcms-client_test.js
@@ -48,7 +48,7 @@ describe('Unit | Steps | modulix-learning content | lcms-client.js', function() 
         {
           id: 'd4c4a2b2-0046-471d-ad9c-15f9cfc8f1f6',
           slug: 'comment-envoyer-un-mail',
-          title: 'Comment envoyer un mail ?',
+          title: 'Comment envoyer un mail ? ',
         },
         {
           id: 'a9e7dda1-fbcc-43a0-a9b4-90056548a4ae',
@@ -61,6 +61,21 @@ describe('Unit | Steps | modulix-learning content | lcms-client.js', function() 
           title: 'À la découverte de l’ENT',
         },
         {
+          id: 'eeeb4951-6f38-4467-a4ba-0c85ed71321a',
+          slug: 'demo-combinix-1',
+          title: 'Demo combinix 1',
+        },
+        {
+          id: 'f32a2238-4f65-4698-b486-15d51935d335',
+          slug: 'demo-combinix-2',
+          title: 'Demo combinix 2',
+        },
+        {
+          id: '235c680e-cbd2-4c56-bef6-80d3ed4d417a',
+          slug: 'demo-epreuves-components',
+          title: 'Démonstration des composants Pix Épreuves',
+        },
+        {
           id: 'ab82925d-4775-4bca-b513-4c3009ec5886',
           slug: 'demo-llm',
           title: 'Démo LLM',
@@ -71,9 +86,39 @@ describe('Unit | Steps | modulix-learning content | lcms-client.js', function() 
           title: 'Les informations sur internet : distinguer le vrai du faux',
         },
         {
+          id: '1b33d6ab-ebfe-49fa-8568-8c40471b4baa',
+          slug: 'galerie',
+          title: 'Galerie Modulix',
+        },
+        {
+          id: '8aa17e6d-3470-479d-838d-ff6923de6686',
+          slug: 'tmp-ia-deepfakes',
+          title: 'Les deepfakes : c’est quoi ?',
+        },
+        {
+          id: '40ab5711-4025-4052-a269-00fd0448d60a',
+          slug: 'tmp-ia-dit-ia',
+          title: 'IA, vous avez dit IA ? ',
+        },
+        {
+          id: 'c8607621-09ab-4601-be7d-68ed9c914c30',
+          slug: 'tmp-ia-hallu',
+          title: 'Elle hallucinent, ces IA génératives ! ',
+        },
+        {
+          id: 'cf183961-e85a-421d-a316-05870191ff82',
+          slug: 'tmp-ia-premier-prompt',
+          title: 'Mon premier prompt !',
+        },
+        {
           id: '875df1ff-27c1-4b41-a0a8-5ff46013f35e',
           slug: 'jeux-video-enfant',
           title: 'Choisir un jeu vidéo adapté à son enfant',
+        },
+        {
+          id: '8343e9c1-5b46-4500-ab85-54061e0c4712',
+          slug: 'les-ia-generatives-consomment',
+          title: 'Les IA génératives, ça consomme !',
         },
         {
           id: '8bdec793-7508-41b4-9df2-e1cf96969680',
@@ -91,34 +136,42 @@ describe('Unit | Steps | modulix-learning content | lcms-client.js', function() 
           title: 'Les principes fondateurs de Wikipédia',
         },
         {
+          id: '7a38c90d-9937-4497-bee3-fe58541af420',
+          slug: 'tmp-prompt-intermediaire',
+          title: 'J\'améliore mes prompts !',
+        },
+        {
           id: '68f92c57-9e66-4258-a2f4-b71fad7ab004',
           slug: 'sources-informations',
           title: 'Les sources d\'information',
         },
         {
-          id: '08ef1a47-b691-4138-b899-39f3512fa152',
-          slug: 'tmp08ef',
-          title: 'Derrière le prompt : comment fonctionnent les IA génératives ?',
+          id: 'bc6d164f-26bd-4f58-9925-8cf5365e2ca6',
+          slug: 'tmp-ia-def-ind',
+          title:
+						'Une IA apprentie botaniste : un cas d\'un apprentissage supervisé 🌱',
         },
         {
-          id: '8aa17e6d-3470-479d-838d-ff6923de6686',
-          slug: 'tmp-ia-deepfakes',
-          title: 'Les deepfakes : c’est quoi ?',
+          id: 'a1eea948-2125-488f-8006-9f85e646830d',
+          slug: 'tmp-ia-bias-ind',
+          title: 'Les biais des lA',
         },
         {
           id: '71618929-fcc9-415e-a3f3-9582545d7a78',
           slug: 'tmp-ia-fonctionnement-debut',
-          title: 'ChatGPT, Gemini, Le Chat : comment font-ils pour répondre à nos demandes ?',
+          title:
+						'Comment font les IA génératives pour répondre à nos demandes ?',
         },
         {
-          id: 'cf183961-e85a-421d-a316-05870191ff82',
-          slug: 'tmp-ia-premier-prompt',
-          title: 'Mon premier prompt !',
+          id: 'd03cef94-74af-463d-8901-c886b48d6e0b',
+          slug: 'tmp-ia-int-mgo',
+          title: 'Comment l\'IA générative apprend-elle à discuter avec vous ?',
         },
         {
-          id: '7a38c90d-9937-4497-bee3-fe58541af420',
-          slug: 'tmp-prompt-intermediaire',
-          title: 'J\'améliore mon prompt !',
+          id: '08ef1a47-b691-4138-b899-39f3512fa152',
+          slug: 'tmp08ef',
+          title:
+						'Derrière le prompt : comment fonctionnent les IA génératives ?',
         },
         {
           id: '19468565-a56b-4aa5-9bf0-369e94bc85ea',
