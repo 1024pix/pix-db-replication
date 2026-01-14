af9928bffca8c1bb488bb38309fd76a72e77edb2
raqqun0101 <nikiforidis.alex@gmail.com>
2026-01-14 19:21:56 +0100

    sanitize execa output

--- src/exec.js
+++ src/exec.js
@@ -1,16 +1,43 @@
 import { execa } from 'execa';
 
+const transform = function * (line) {
+
+  line = sanitizeCredentials(line);
+
+  yield line;
+
+};
+
+function sanitizeCredentials(text) {
+  const db_credentials_regex = /postgres:\/\/([\w]+:[\w]+)/gi;
+  return text.replace(db_credentials_regex, 'postgres://*:*');
+}
+
 async function execStdOut(cmd, args) {
-  const { stdout } = await execa({ stderr: 'inherit' }) `${cmd} ${args}`;
-  return stdout;
+  try {
+    const { stdout } = await execa({ stdout: transform, stderr: 'inherit' })`${cmd} ${args}`;
+    return stdout;
+  } catch (error) {
+    throw new Error(sanitizeCredentials(error.shortMessage));
+  }
 }
 
-function execShell(cmdline) {
-  return execa({ stdio: 'inherit', shell: true }) `${cmdline}`;
+async function execShell(cmdline) {
+  try {
+    const { stdout } = await execa({ stdout: transform, stderr: 'inherit', shell: true })`${cmdline}`;
+    return stdout;
+  } catch (error) {
+    throw new Error(sanitizeCredentials(error.shortMessage));
+  }
 }
 
-function exec(cmd, args) {
-  return execa({ stdio: 'inherit' }) `${cmd} ${args}`;
+async function exec(cmd, args) {
+  try {
+    const { stdout } = await execa({ stdout: transform, stderr: 'inherit' }) `${cmd} ${args}`;
+    return stdout;
+  } catch (error) {
+    throw new Error(sanitizeCredentials(error.shortMessage));
+  }
 }
 
 export {
