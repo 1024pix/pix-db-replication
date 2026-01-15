97b142f824cd0ffe60ada6727d4b418797bfdba0
raqqun0101 <nikiforidis.alex@gmail.com>
2026-01-15 10:52:04 +0100

    use stdout to transform

--- src/exec.js
+++ src/exec.js
@@ -24,7 +24,7 @@ async function execStdOut(cmd, args) {
 
 async function execShell(cmdline) {
   try {
-    const { stdout } = await execa({ stdout: transform, stderr: 'inherit', shell: true })`${cmdline}`;
+    const { stdout } = await execa({ stdout: [transform], stderr: 'inherit', shell: true })`${cmdline}`;
     return stdout;
   } catch (error) {
     throw new Error(sanitizeCredentials(error.shortMessage));
@@ -33,7 +33,7 @@ async function execShell(cmdline) {
 
 async function exec(cmd, args) {
   try {
-    const { stdout } = await execa({ stdout: transform, stderr: 'inherit' }) `${cmd} ${args}`;
+    const { stdout } = await execa({ stdout: [transform] }) `${cmd} ${args}`;
     return stdout;
   } catch (error) {
     throw new Error(sanitizeCredentials(error.shortMessage));
